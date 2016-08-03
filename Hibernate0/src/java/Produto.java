ef4324b9a854aac514432b2c0a2b945083bbcd1c
Jonathan Galdino de Oliveira <RA21506739@uniceub.edu.br>
2016-08-02 22:25:37 -0300

    salvar

---
+++ Hibernate0/src/java/Produto.java
@@ -0,0 +1,49 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ra21506739
+ */
+@Entity
+public class Produto {
+    
+    @Id    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    private String descricao;
+    private float preco;
+
+
+    
+}
---
+++ Hibernate01/src/java/Produto.java
@@ -0,0 +1,37 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Produto {
+    @Id
+    private String nome;
+    private String descricao;
+    private float preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+}
